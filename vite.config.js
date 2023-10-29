d99644e2944a766affaca1b63f4f6256172e9b28
rsu <61874914+rsu-Suba@users.noreply.github.com>
2023-10-29 22:26:34 +0900

    fix vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from "vite"; 
 
 export default defineConfig({
-  //base:'./PixelCameraUnit/',
-  base:'',
+  base:'./PixelCameraUnit/',
+  //base:'',
   root: "src",
   build: {
     outDir:"../docs",
